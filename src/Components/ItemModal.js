a057461f0797f6864080fd817abeb3cc3a5d8a3e
Moses-Yu <mosesyumss@gmail.com>
2023-07-14 15:00:36 +0900

    [#6] css 작성중

--- src/Components/ItemModal.js
+++ src/Components/ItemModal.js
@@ -1,14 +1,14 @@
 import React from "react";
-
+import ItemModalStyle from "./styles/ItemModal.styled";
 const ItemModal = ({ name, bookmark, image_url }) => {
   return (
-    <div>
+    <ItemModalStyle>
       <div>
         <img src={image_url} alt=""/>
         <img alt="close" src="/images/ic-round-close.svg"></img>
         <div>{name}</div>
       </div>
-    </div>
+    </ItemModalStyle>
   );
 };
 
--- src/Components/itemModal.stories.js
+++ src/Components/itemModal.stories.js
@@ -9,17 +9,8 @@ export default {
 
 export const Basic = {
   args: {
-    item: {
-      id: 0,
-      type: "Product",
-      title: "두루마리 휴지",
-      sub_title: null,
-      brand_name: null,
-      price: 2500,
-      discountPercentage: 35,
-      image_url: "/images/randomimage/1.png",
-      brand_image_url: null,
-      follower: null,
-    },
+    name: "두루마리 휴지",
+    bookmark: false,
+    image_url: "/images/randomimage/1.png"
   },
 };
