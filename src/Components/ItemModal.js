f700ae5ad70ad5d9ce2d7829d2867cd4fc7089b7
Moses-Yu <mosesyumss@gmail.com>
2023-07-14 17:55:05 +0900

    [#6] 모달창 외부 클릭시 모달창 끄기

--- src/Components/ItemModal.js
+++ src/Components/ItemModal.js
@@ -3,8 +3,8 @@ import ItemModalStyle from "./styles/ItemModal.styled";
 const ItemModal = ({ name, bookmark, image_url, setModalIsOpen }) => {
   const handleClick = () => setModalIsOpen(false)
     return (
-    <ItemModalStyle image_url={image_url} >
-      <div className="container">
+    <ItemModalStyle image_url={image_url} onClick={handleClick} >
+      <div className="container" onClick={(event) => event.stopPropagation()}>
         <img
           className="close"
           alt="close"
@@ -12,6 +12,7 @@ const ItemModal = ({ name, bookmark, image_url, setModalIsOpen }) => {
           onClick={handleClick}
         ></img>
         <div className="info">{name}</div>
+        <div className="shadow"></div>
       </div>
     </ItemModalStyle>
   );
