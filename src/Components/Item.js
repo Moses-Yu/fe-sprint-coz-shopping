95d88d9d81c45e0498d1808fbfc4b04e2d8f9404
Moses-Yu <mosesyumss@gmail.com>
2023-07-14 17:33:49 +0900

    [#6] 모달창 구현

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -1,49 +1,79 @@
-import React from "react";
-import ItemWrapper from "./styles/Item.styled"
+import React, { useEffect, useState } from "react";
+import ItemWrapper from "./styles/Item.styled";
 import { BRAND, PRODUCT, CATEGORY } from "./Type";
+import ItemModal from "./ItemModal";
 
 const Item = ({ item }) => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [info, setInfo] = useState({});
+  const [loaded, setLoaded] = useState(false);
+
+  const handleClick = () => setModalIsOpen(true)
+
+  const sortData = () => {
+    const info = {};
+    info.img_alt =
+      item.type === BRAND
+        ? item.brand_name
+        : item.type === CATEGORY
+        ? "# " + item.title
+        : item.title;
+    info.img_url = item.type === BRAND ? item.brand_image_url : item.image_url;
+    info.name =
+      item.type === BRAND
+        ? item.brand_name
+        : item.type === CATEGORY
+        ? "# " + item.title
+        : item.title;
+    info.css = item.type === BRAND ? "brand" : null;
+    info.top_right_text =
+      item.type === BRAND
+        ? "관심고객수"
+        : item.discountPercentage
+        ? item.discountPercentage + "%"
+        : null;
+    info.subtitle = item.sub_title;
+    info.bottom_right_text =
+      item.type === BRAND
+        ? item.follower
+        : item.type === PRODUCT
+        ? item.price + "원"
+        : null;
+    setInfo(info);
+  };
+  useEffect(() => {
+    setLoaded(false);
+    sortData();
+    setLoaded(true);
+  });
+
   return (
-    <ItemWrapper>
-      <img
-        alt={
-          item.type === BRAND
-            ? item.brand_name
-            : item.type === CATEGORY
-            ? "# " + item.title
-            : item.title
-        }
-        src={item.type === BRAND ? item.brand_image_url : item.image_url}
-      ></img>
-      <div className="info">
-        <div className="info_top">
-          <div className="top_left">
-            {item.type === BRAND
-              ? item.brand_name
-              : item.type === CATEGORY
-              ? "# " + item.title
-              : item.title}
+    <>
+      {loaded ? (
+        <ItemWrapper onClick={handleClick}>
+          <img alt={info.img} src={info.img_url}></img>
+          <div className="info">
+            <div className="info_top">
+              <div className="top_left">{info.name}</div>
+              <div className={`top_right ${info.css}`}>
+                {info.top_right_text}
+              </div>
+            </div>
+            <div className="info_bottom">
+              <div className="bottom_left">{info.subtitle}</div>
+              <div className="bottom_right">{info.bottom_right_text}</div>
+            </div>
           </div>
-          <div className={`top_right ${item.type === BRAND ? "brand" : null}`}>
-            {item.type === BRAND
-              ? "관심고객수"
-              : item.discountPercentage
-              ? item.discountPercentage + "%"
-              : null}
-          </div>
-        </div>
-        <div className="info_bottom">
-          <div className="bottom_left">{item.sub_title}</div>
-          <div className="bottom_right">
-            {item.type === BRAND
-              ? item.follower
-              : item.type === PRODUCT
-              ? item.price + "원"
-              : null}
-          </div>
-        </div>
-      </div>
-    </ItemWrapper>
+        </ItemWrapper>
+      ) : null}
+      {modalIsOpen && (
+        <ItemModal
+          name={info.name}
+          image_url={info.img_url}
+          setModalIsOpen={setModalIsOpen}
+        />
+      )}
+    </>
   );
 };
 
--- src/Components/styles/ItemModal.styled.js
+++ src/Components/styles/ItemModal.styled.js
@@ -1,9 +1,58 @@
-import styled from "styled-components"
+import styled from "styled-components";
 const ItemModalStyle = styled.div`
-    background: rgba(255, 255, 255, 0.40);
-    position: fixed;
-    width: 100%;
-    height: 100%;
-`
+  background: rgba(255, 255, 255, 0.4);
+  position: fixed;
+  inset: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-export default ItemModalStyle
+  .container {
+    display: flex;
+    width: 46.5rem;
+    height: 30rem;
+    border-radius: 0.75rem;
+    box-shadow: 0px 0px 36px 0px rgba(0, 0, 0, 0.5);
+    background-image: url(${(props) => props.image_url});
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    stroke-width: 1px;
+    stroke: rgba(0, 0, 0, 0.1);
+  }
+  .info {
+    position: absolute;
+    display: flex;
+    width: fit-content;
+    height: 1.875rem;
+    flex-direction: column;
+    justify-content: center;
+
+    flex-shrink: 0;
+    margin-left: 3.5rem;
+    margin-top: 26.81rem;
+
+    width: 41.5rem;
+    height: 1.875rem;
+    flex-direction: column;
+    justify-content: center;
+    flex-shrink: 0;
+    color: #fff;
+    text-shadow: 0px 0px 4px 0px rgba(0, 0, 0, 0.1);
+    font-family: Inter;
+    font-size: 1.5rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+  }
+  .close {
+    position: absolute;
+    width: 1.5rem;
+    height: 1.5rem;
+    flex-shrink: 0;
+    margin-left: 43.5rem;
+    margin-top: 1.5rem;
+  }
+`;
+
+export default ItemModalStyle;
